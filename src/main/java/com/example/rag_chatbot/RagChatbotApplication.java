b3bce4b18a9eb5e4f41c098b082f88f8c6e9f460
Venkatasai247 <udathavenkata.sai@healthmedpro.com>
2026-06-16 09:47:28 +0530

    Fixed

--- src/main/java/com/example/rag_chatbot/RagChatbotApplication.java
+++ src/main/java/com/example/rag_chatbot/RagChatbotApplication.java
@@ -1,10 +1,13 @@
 package com.example.rag_chatbot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class RagChatbotApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RagChatbotApplication.class, args);
